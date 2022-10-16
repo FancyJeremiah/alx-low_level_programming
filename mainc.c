47f5109165e23ac54783d75f5f80b275fe1747e3
FancyJeremiah <fancyjeremyn@gmail.com>
2022-10-16 06:54:37 -0700

    6-zise.c

---
+++ mainc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+int main(void)
+{
+	int k;
+        long int l;
+        long long int m;
+	char n;		
+	float f
+	printf("Size of a char: %x byte(s)\n", (unsigned long)sizeof(n));
+	printf("Size of an int: %x byte(s)\n", (unsigned long)sizeof(k));
+	printf("Size of a long int: %x byte(s)\n", (unsigned long)sizeof(l));  
+	printf("Size of a long long int: %x byte(s)\n", (unsigned long)sizeof(m));
+	printf("Size of a float: %x byte(s)\n", (unsigned long)sizeof(f));
+
+	return (0);
+
+}

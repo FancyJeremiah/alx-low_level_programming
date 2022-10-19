f138bd132831ccb7cab0f848f36f0d28c5aa532a
FancyJeremiah <fancyjeremyn@gmail.com>
2022-10-19 08:31:16 -0700

    0-positive_or_negative.c

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+/*
+ *
+ * Return 0 (Sucess);
+ *
+ */
+int main(void)
+{
+	int number = -7;
+
+	if (number >= 0)
+
+		 printf("%d is positive\n", number);
+
+	   else
+
+                 printf("%d is negative\n", number);
+
+	   
+	 return 0;
+}
+
